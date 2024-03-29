4381daffdf228a3541239368fac076210e00b525
KOGANTI VENKATA  ABHISHEK <120243311+VenkataAbhishek@users.noreply.github.com>
2024-03-29 12:08:11 +0530

    Update login.js

--- login.js
+++ login.js
@@ -4,5 +4,5 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
     // Perform login authentication
     // Redirect to index.html upon successful login
-    window.location.href = 'userinter.html';
+    window.location.href = 'index.html';
 });
